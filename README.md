31bda2af20bbb6dc9dfaeaf3c9a64985b099a739
GuilhermeSK2 <iosfantasma08@gmail.com>
2025-01-26 22:38:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Client-Credentials
+OAuth Client Credentials com Java, Spring Boot e Keycloak (Com RestClient)
---
+++ client-service/src/main/java/br/com/guilhermefreitas/client_service/RestClientOauthConfig.java
@@ -0,0 +1,24 @@
+package br.com.guilhermefreitas.client_service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientOauthConfig {
+
+  @Bean
+  RestClient keycloakRestClientOauth(RestClient.Builder builder,
+      OAuth2AuthorizedClientManager authorizedClientManager) {
+    OAuth2ClientHttpRequestInterceptor requestInterceptor = new OAuth2ClientHttpRequestInterceptor(
+        authorizedClientManager);
+    requestInterceptor.setClientRegistrationIdResolver((HttpRequest request) -> "client-service");
+
+    return builder
+        .requestInterceptor(requestInterceptor)
+        .build();
+  }
+}
